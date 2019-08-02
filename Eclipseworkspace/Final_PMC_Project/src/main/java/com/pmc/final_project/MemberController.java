1590d370016db30e996211b5b327d649271a5976
khm1580 <khm15801@gmail.com>
2019-08-02 09:34:39 +0900

    no message

--- Eclipseworkspace/Final_PMC_Project/src/main/java/com/pmc/final_project/MemberController.java
+++ Eclipseworkspace/Final_PMC_Project/src/main/java/com/pmc/final_project/MemberController.java
@@ -42,7 +42,7 @@ public class MemberController {
 	
 	@RequestMapping(value="/MemberInfo")
 	public ModelAndView MemberInfo(Integer pageNum) {
-		mav = mm.getmemberInfo(pageNum);
+//		mav = mm.getmemberInfo(pageNum);
 		
 		return mav;
 	}
---
+++ Eclipseworkspace/Final_PMC_Project/src/main/java/com/pmc/final_project/dao/ICateDao.java
@@ -0,0 +1,11 @@
+package com.pmc.final_project.dao;
+
+import java.util.List;
+
+import com.pmc.final_project.bean.ProductCate;
+
+public interface ICateDao {
+
+	List<ProductCate> getCateList();
+
+}
